25ec6f5cec4d88e9717292b07f2e843b86a3a510
Zacharia Lutz <zacharia.lutz@gmail.coom>
2019-10-09 13:13:18 -0500

    bomb component working

--- src/state-drills/Bomb.js
+++ src/state-drills/Bomb.js
@@ -4,25 +4,33 @@ import React from 'react';
 export default class Bomb extends React.Component {
 
   state = {
-    count: 0
+    count: 0,
+    timer: null
   }
 
   componentDidMount() {
-    setInterval(() => {
-      this.setState(
-        {
-        count: this.state.count + 1
-      }
-    )}, 1000)
+    this.setState({
+      timer: setInterval(() => {
+        this.setState(
+          {count: this.state.count + 1})
+      }, 1000)
+    })
   }
 
+  render() {
+    let bombstate;
+    if (this.state.count === 8) {
+      bombstate = 'BOOM!!!!';
+      clearInterval(this.state.timer);
+    }
+  		else if (this.state.count % 2 === 0) { bombstate = 'tick' }
+    else { bombstate = 'tock' }
   
 
-  render() {
     return (
       <div>
         <p>
-          {this.state.bombstate}
+          {bombstate}
         </p>
       </div>
     )
